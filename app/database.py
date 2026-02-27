cb30f191688e4500793b9570dd52b5c0923b5f11
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-02-28 00:43:36 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,22 @@
+import os
+import psycopg2
+from urllib.parse import urlparse
 
+def get_db():
+    database_url = os.environ.get("DATABASE_URL")
+
+    if not database_url:
+        raise Exception("DATABASE_URL not found in environment variables")
+
+    result = urlparse(database_url)
+
+    conn = psycopg2.connect(
+        database=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port,
+        sslmode="require"
+    )
+
+    return conn
