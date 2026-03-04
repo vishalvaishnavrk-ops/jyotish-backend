0baec2cd7244aab457def67d8dc1b8f26cbf9574
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-03-04 18:19:42 +0530

    Update helpers.py

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,1 +1,8 @@
+import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+def generate_client_code():
+    year = datetime.now(ZoneInfo("Asia/Kolkata")).year
+    short_unique = int(time.time()) % 100000
+    return f"AVV-{year}-{short_unique}"
