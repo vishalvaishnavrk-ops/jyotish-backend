bc9d3658d6c0d6a5c84c4528a85ffdf12f1a7af7
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-03-04 18:25:14 +0530

    Update admin.py

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.models import get_clients
+from app.database import get_db
+from app.services.ai_engine import generate_ai_draft
+from app.services.pdf_engine import generate_pdf_report
+from app.services.whatsapp_service import generate_whatsapp_link
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
@@ -33,3 +38,45 @@ def dashboard():
         html += f"{r}<br>"
 
     return html
+
+@router.get("/admin/client/{client_id}")
+def client_detail(client_id:int):
+
+    conn = get_db()
+    c = conn.cursor()
+
+    c.execute("SELECT id,client_code,name,phone,ai_draft,status FROM clients WHERE id=%s",(client_id,))
+    data = c.fetchone()
+
+    conn.close()
+
+    return data
+
+@router.post("/admin/client/{client_id}/generate-ai")
+def ai_generate(client_id:int):
+
+    generate_ai_draft(client_id)
+
+    return {"status":"AI generated"}
+
+@router.post("/admin/client/{client_id}/generate-pdf")
+def pdf_generate(client_id:int):
+
+    file = generate_pdf_report(client_id)
+
+    return {"pdf":file}
+
+@router.get("/admin/client/{client_id}/pdf")
+def download_pdf(client_id:int):
+
+    conn=get_db()
+    c=conn.cursor()
+
+    c.execute("SELECT client_code FROM clients WHERE id=%s",(client_id,))
+    code=c.fetchone()[0]
+
+    conn.close()
+
+    path=f"reports/{code}.pdf"
+
+    return FileResponse(path, media_type="application/pdf")
