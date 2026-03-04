a0c16c080833c862d1f22760fa8d7662464b0e12
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-03-04 18:50:38 +0530

    Update website.py

--- app/routes/website.py
+++ app/routes/website.py
@@ -1,19 +1,43 @@
-from fastapi import APIRouter, Form
-from app.models import create_client
+from fastapi import APIRouter, Form, UploadFile, File
+from typing import List
 import uuid
+import os
 from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from app.models import create_client
+from app.utils.helpers import generate_client_code
+
+UPLOAD_DIR = "uploads"
 
 router = APIRouter()
 
-def generate_client_code():
-    return "AVV-" + str(uuid.uuid4())[:8]
 
 @router.post("/api/website-submit")
-def website_submit(
+async def website_submit(
     name: str = Form(...),
     phone: str = Form(...),
-    plan: str = Form(...)
+    dob: str = Form(...),
+    questions: str = Form(...),
+    plan: str = Form(...),
+    tob: str = Form(None),
+    place: str = Form(None),
+    images: List[UploadFile] = File(...)
 ):
+
+    saved_files = []
+
+    for img in images:
+
+        unique_name = f"{uuid.uuid4().hex}_{img.filename}"
+        path = os.path.join(UPLOAD_DIR, unique_name)
+
+        with open(path, "wb") as f:
+            f.write(await img.read())
+
+        saved_files.append(unique_name)
+
+    image_names = ",".join(saved_files)
 
     client_code = generate_client_code()
 
@@ -21,20 +45,23 @@ def website_submit(
         client_code,
         name,
         phone,
-        None,
-        None,
-        None,
+        dob,
+        tob,
+        place,
         plan,
-        "",
-        "",
+        questions,
+        image_names,
         "Website",
         "Pending",
         "Pending",
-        datetime.now(),
+        datetime.now(ZoneInfo("Asia/Kolkata")),
         99,
         0
     )
 
     create_client(data)
 
-    return {"success": True, "client_code": client_code}
+    return {
+        "success": True,
+        "client_code": client_code
+    }
