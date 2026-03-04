40678a5cfb1a0bc799c78530fecdf482c7376626
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-03-04 17:29:42 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,35 +1,15 @@
-from datetime import datetime
-from zoneinfo import ZoneInfo
-from .database import get_db
+from app.database import get_db
 
-def create_client(client_code, name, phone, plan):
+def create_client(data):
     conn = get_db()
-    cur = conn.cursor()
+    c = conn.cursor()
 
-    cur.execute("""
-        INSERT INTO clients
-        (client_code, name, phone, plan, source, status, payment_status, created_at, priority, ai_generated)
-        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    """, (
-        client_code,
-        name,
-        phone,
-        plan,
-        "Manual",
-        "Pending",
-        "Pending",
-        datetime.now(ZoneInfo("Asia/Kolkata")),
-        99,
-        0
-    ))
+    c.execute("""
+    INSERT INTO clients
+    (client_code,name,phone,dob,tob,place,plan,questions,images,
+     source,status,payment_status,created_at,priority,ai_generated)
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """, data)
 
     conn.commit()
     conn.close()
-
-def get_all_clients():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT id, client_code, name, phone, plan, status FROM clients ORDER BY id DESC")
-    rows = cur.fetchall()
-    conn.close()
-    return rows
