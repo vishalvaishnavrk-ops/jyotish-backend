3a0a8d73743b597ea69a1204afbace87d2dffc4d
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-03-20 00:38:47 +0530

    Update generate_hash.py

--- generate_hash.py
+++ generate_hash.py
@@ -1,10 +1,3 @@
-from passlib.context import CryptContext
+from passlib.hash import bcrypt
 
-pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-password = "admin123"
-
-hashed = pwd.hash(password)
-
-print("YOUR HASH:")
-print(hashed)
+print(bcrypt.hash("admin123"))
