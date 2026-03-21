1d2f5f1b2b5e4004d46584d114c430db964b158b
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-03-22 02:52:30 +0530

    Update supabase_storage.py

--- app/services/supabase_storage.py
+++ app/services/supabase_storage.py
@@ -22,3 +22,12 @@ def upload_palm_image(file_bytes, filename):
     public_url = f"{SUPABASE_URL}/storage/v1/object/public/{BUCKET_NAME}/{path}"
 
     return public_url
+
+def upload_pdf(file_bytes, file_name):
+    path = f"reports/{file_name}"
+
+    supabase.storage.from_("reports").upload(path, file_bytes, {"upsert": False})
+
+    public_url = supabase.storage.from_("reports").get_public_url(path)
+
+    return public_url
