fd42c83dd312d7cf1cc29935edf609591c464017
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-04-06 00:26:07 +0530

    Update supabase_storage.py

--- app/services/supabase_storage.py
+++ app/services/supabase_storage.py
@@ -9,20 +9,22 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 BUCKET_NAME = "palms"
 
 
-def upload_palm_image(file_bytes, filename):
-
-    path = f"client_images/{filename}"
+def upload_palm_image(file_bytes, filename, client_code):
+    path = f"client_images/{client_code}/{filename}"
 
     supabase.storage.from_(BUCKET_NAME).upload(
         path,
         file_bytes,
-        {"content-type": "image/jpeg"}
+        {
+            "content-type": "image/jpeg",
+            "upsert": False
+        }
     )
 
     public_url = f"{SUPABASE_URL}/storage/v1/object/public/{BUCKET_NAME}/{path}"
 
     return public_url
-
+    
 def upload_pdf(file_bytes, file_name):
     path = f"reports/{file_name}"
 
