38c903a390a2ef935518c8f16d0b83aaa9b490a9
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-04-06 21:55:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,8 +11,14 @@ app = FastAPI()
 
 load_dotenv()
 
-app.add_middleware(SessionMiddleware, secret_key="mysecret123")
-
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="mysecret123",
+    session_cookie="admin_session",
+    max_age=None,   # 🔥 browser close पर expire
+    same_site="lax",
+    https_only=False  # production में True करना
+)
 
 # ---------- FOLDERS ----------
 UPLOAD_DIR = "uploads"
