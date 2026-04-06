c08e4267cb77c7fe36ed91cbe6f49490f538b58d
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-04-06 12:46:25 +0530

    Update admin.py

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -13,8 +13,6 @@ from app.utils.helpers import generate_client_code
 from app.services.ai_engine import generate_ai_draft
 from app.services.pdf_engine import generate_pdf_report
 from app.services.supabase_storage import upload_palm_image
-from app.services.supabase_storage import supabase
-from app.services.supabase_storage import SUPABASE_URL
 from fastapi.templating import Jinja2Templates
 from jinja2 import StrictUndefined   # 👈 ADD THIS
 
