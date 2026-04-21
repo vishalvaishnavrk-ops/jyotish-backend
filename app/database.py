6a75959741c69cda781e1201de13199b122077f0
vishalvaishnavrk-ops <vishalvaishnavrk@gmail.com>
2026-04-21 16:55:28 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,15 +1,19 @@
 import psycopg2
+from psycopg2 import pool
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 🔥 GLOBAL CONNECTION (REUSE)
-conn = None
+# 🔥 CONNECTION POOL (MAIN FIX)
+db_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dsn=DATABASE_URL,
+    sslmode="require"
+)
 
 def get_db():
-    global conn
+    return db_pool.getconn()
 
-    if conn is None or conn.closed != 0:
-        conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-
-    return conn
+def release_db(conn):
+    db_pool.putconn(conn)
